d10f18ef092c44661aad8a3c2450d248a2165846
Rodrigo Pedreira <rodrigormp2@hotmail.com>
2026-09-19 01:15:31 -0300

    Padrao do django mais app e modelos iniciais

---
+++ gestorproj/gestor/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Aluno(models.Model):
+    ra = models.CharField(max_length=10, unique=True)
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
+class Atividade(models.Model):
+    nome = models.CharField(max_length=100)
+    alunos = models.ManyToManyField(Aluno, blank=True, related_name="atividades")
+
+    class Meta:
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
