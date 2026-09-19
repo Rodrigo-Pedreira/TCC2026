03a1567befcc62971d259bcbb3b5ce389db25b68
Rodrigo Pedreira <rodrigormp2@hotmail.com>
2026-09-19 13:11:32 -0300

    Views temporarias

--- gestorproj/gestor/urls.py
+++ gestorproj/gestor/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "gestor"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("alunos/", views.alunos, name="alunos"),
+    path("atividades/", views.atividades, name="atividades"),
+    path("aluno/<str:aluno_ra>/", views.aluno, name="aluno"),
+    path("atividade/<str:atividade_nome>/", views.atividade, name="atividade")
 ]
--- gestorproj/gestor/views.py
+++ gestorproj/gestor/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Aluno, Atividade
+
+# TODO: Numero de total de alunos e atividades
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    total_alunos = Aluno.objects.count()
+    total_atividades = Atividade.objects.count()
+    context = {'total_alunos': total_alunos, 'total_atividades': total_atividades}
+    return render(request, "gestor/index.html", context)
+
+def alunos(request):
+    alunos = Aluno.objects.all()
+    context = {'alunos': alunos}
+    return render(request, "gestor/alunos.html", context)
+
+
+def aluno(request, aluno_ra):
+    aluno = get_object_or_404(Aluno,ra=aluno_ra)
+    aluno_atividades = [at.nome for at in aluno.atividades.all()] # pyright: ignore[reportAttributeAccessIssue]
+    context = {'aluno': aluno, 'aluno_atividades': aluno_atividades}
+    return render(request, "gestor/aluno.html", context)
+
+def atividade(request, atividade_nome):
+    atividade = get_object_or_404(Atividade, nome=atividade_nome)
+    alunos = atividade.alunos.all()
+    context = {'atividade': atividade, 'alunos': alunos}
+    return render(request, "gestor/atividade.html", context)
+
+def atividades(request):
+    atividades = Atividade.objects.all()
+    context = {'atividades': atividades}
+    return render(request, "gestor/atividades.html", context)
