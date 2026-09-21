0a631198b29df171fa7372044d0a9e0c4015f065
Rodrigo Pedreira <rodrigormp2@hotmail.com>
2026-09-21 11:43:13 -0300

    fuzzy search

--- gestorproj/gestorsite/settings.py
+++ gestorproj/gestorsite/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gestor.apps.GestorConfig',
+    "gestor.apps.GestorConfig",
+    "django.contrib.postgres",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
