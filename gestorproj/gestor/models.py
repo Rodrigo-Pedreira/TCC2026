f8aba7afbfddfb9e36f149306569cb5027e0f08a
Rodrigo Pedreira <rodrigormp2@hotmail.com>
2026-09-21 11:42:45 -0300

    validador de ra

--- gestorproj/gestor/models.py
+++ gestorproj/gestor/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Aluno(models.Model):
-    ra = models.CharField(max_length=10, unique=True)
+    ra = models.CharField(
+        max_length=10,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"^\d{2}\.\d{5}-\d$",
+                message="O RA deve estar no formato 00.00000-0.",
+                code="RA_invalido",
+            )
+        ],
+    )
     nome = models.CharField(max_length=100)
 
     class Meta:
